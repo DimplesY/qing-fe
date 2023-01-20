88c083a2040a3ad036f396f786524b2e74caeb00
Dimples_YJ <2890841438@qq.com>
2023-01-20 22:39:07 +0800

    feat: 增加tailwind配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
+  extend: {
+    fontFamily: {
+      primary: ['Inter', ...fontFamily.sans],
+    },
+  },
 }
